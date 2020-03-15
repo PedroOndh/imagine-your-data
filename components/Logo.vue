7587fc3b6302a32af4273b7178d61a11accaae03
Adrián <adri.arenal94@gmail.com>
2020-03-15 23:55:01 +0100

    Fixing issue of different formatters IDE/Prettier

--- components/Logo.vue
+++ components/Logo.vue
@@ -24,17 +24,17 @@
   </svg>
 </template>
 <style>
-  .NuxtLogo {
-    animation: 1s appear;
-    margin: auto;
-  }
+.NuxtLogo {
+  animation: 1s appear;
+  margin: auto;
+}
 
-  @keyframes appear {
-    0% {
-      opacity: 0;
-    }
-    100% {
-      opacity: 1;
-    }
+@keyframes appear {
+  0% {
+    opacity: 0;
   }
+  100% {
+    opacity: 1;
+  }
+}
 </style>
