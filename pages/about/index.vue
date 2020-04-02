36813c5c70178a10a1e48ec597e92176957dd1f9
Pedro Ondiviela <pedroo@empathy.co>
2020-04-02 11:27:56 +0200

    making about editable and adding authors to collections

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,10 +1,20 @@
 <template>
-  <p>About imagine your data</p>
+  <div>
+    <h1>{{ content.attributes.title }}</h1>
+    <!-- eslint-disable-next-line -->
+    <div v-html="content.html" />
+  </div>
 </template>
 
 <script>
 export default {
-  layout: 'page'
+  layout: 'page',
+  async asyncData() {
+    const content = await import(`~/content/pages/about.md`)
+    return {
+      content
+    }
+  }
 }
 </script>
 
