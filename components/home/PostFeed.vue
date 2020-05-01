38bcc83ada4a2e70ed0020cb84c83bea19dff1e4
Pedro Ondiviela <pedroo@empathy.co>
2020-05-01 12:39:08 +0200

    feat(style): corrections on media queries

--- components/home/PostFeed.vue
+++ components/home/PostFeed.vue
@@ -34,14 +34,14 @@ section {
   background-color: $grey-background;
   &.posts--filtering {
     padding-top: 20rem;
-    margin-top: -5rem;
+    margin-top: 0;
   }
   .posts-feed {
     position: relative;
     top: -8.5vw;
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
-    grid-auto-rows: 16.3vw;
+    grid-auto-rows: rem(313px);
     transition: all 1s ease;
 
     .posts-feed-item {
@@ -108,12 +108,12 @@ section {
       }
     }
   }
-  @media screen and (min-width: $breakpoint__large-desktop--min) {
+  @media screen and (max-width: $breakpoint__desktop--max) {
     .posts-feed {
-      grid-auto-rows: rem(313px);
+      grid-auto-rows: 16.3vw;
     }
     &.posts--filtering {
-      margin-top: 0;
+      margin-top: -5rem;
     }
   }
   @media screen and (max-width: $breakpoint__small-desktop--max) {
